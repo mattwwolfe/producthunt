75a811b7c51b6fdd99b826231b708158ae747259
Matt Wolfe <matt.w.wolfe@gmail.com>
2018-09-08 14:35:11 -0500

    built out the home page with links to the details page

--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html', {'products':products})
 
 @login_required
 def create(request):
@@ -22,9 +23,20 @@ def create(request):
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
             product.save()
-            return redirect('home')
+            return redirect('/products/' + str(product.id))
 
         else:
             return render(request, 'products/create.html', {'error':'all fields are required'})
     else:
         return render(request, 'products/create.html')
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product':product})
+
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product_id))
