9ec41cdebb769efb38f7e3abd40642e8a6ab8b11
Matt Wolfe <matt.w.wolfe@gmail.com>
2018-09-08 12:31:27 -0500

    Added ability to add products via products/create page

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,10 +1,12 @@
 from products import views
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
